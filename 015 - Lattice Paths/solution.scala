35af17acb032e247bd5dcf5b2a6475a580251856
Travis Parks <jehugaleahsa@gmail.com>
2015-06-01 16:26:37 -0400

    Optimized Problem 015

--- 015 - Lattice Paths/solution.scala
+++ 015 - Lattice Paths/solution.scala
@@ -1,27 +1,27 @@
 package euler
 
 object Solution extends App {
-
-	import scala.collection.mutable.Map
 	
-	val resultLookup = Map[(Long, Long), Long]()
-
-	def tryPath(x: Long, y: Long, maxX: Long, maxY: Long) : Long = {
-		if (resultLookup.contains((x, y))) resultLookup((x, y))
-		else if (x == maxX && y == maxY) 1L // we made it to the end, this path succeeded
-		else if (x > maxX) 0L // this path goes beyond the right barrier, so it fails
-		else if (y > maxY) 0L // this path goes beyond the bottom barrier, so it fails
-		else {
-			val right = tryPath(x + 1, y, maxX, maxY)
-			val down = tryPath(x, y + 1, maxX, maxY)
-			val sum = right + down
-			resultLookup((x, y)) = sum
-			sum
-		}
-	}
-	
-	val maxX = 20
+    val maxX = 20
 	val maxY = 20
-	val totalPaths = tryPath(0, 0, maxX, maxY)
+	val resultLookup = scala.collection.mutable.IndexedSeq.fill[Long](maxX, maxY)(0L)
+    
+    def cache(x: Int, y: Int, f: (Int, Int) => Long): Long = {
+        val result = f(x, y)
+        if (x < maxX && y < maxY) {
+            resultLookup(x)(y) = result
+        }
+        result
+    }
+
+	def tryPath(x: Int, y: Int): Long = {
+		if (x == maxX && y == maxY) 1L // we made it to the end, this path succeeded
+        else if (x == maxX) cache(x, y + 1, tryPath _)
+        else if (y == maxY) cache(x + 1, y, tryPath _)
+        else if (resultLookup(x)(y) != 0L) resultLookup(x)(y)
+		else cache(x + 1, y, tryPath _) + cache(x, y + 1, tryPath _)
+	}
+
+	val totalPaths = tryPath(0, 0)
 	println(totalPaths)
 }
--- 015 - Lattice Paths/test.ps1
+++ 015 - Lattice Paths/test.ps1
@@ -1,2 +1,4 @@
+Write-Host "Compiling..."
 &scalac solution.scala
+Write-Host "Running..."
 &scala -J-Xmx2g euler.Solution
