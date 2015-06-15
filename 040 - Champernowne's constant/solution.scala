67406d3b6885d9f6bc8319fd838ac95e6cf656e1
Travis Parks <jehugaleahsa@gmail.com>
2015-06-15 09:06:20 -0400

    Problem 040 - Removed Unused Code

--- 040 - Champernowne's constant/solution.scala
+++ 040 - Champernowne's constant/solution.scala
@@ -1,19 +1,6 @@
 package euler
 
 object Solution extends App {
-
-    def getOffset(value: Int): Int = {
-        val nine = 9
-        val power = scala.math.log10(value).toInt        
-        val numsPerPower = (0 until power)
-            .map(p => (p, nine * scala.math.pow(10, p).toInt))        
-        val leadingNums = numsPerPower.map(p => p._2).sum    
-        val leadingDigits = numsPerPower.map(p => (p._1 + 1) * p._2).sum    
-        val remainingNums = value - leadingNums - 1    
-        val remainingDigits = remainingNums * (power + 1)    
-        val offset = leadingDigits + remainingDigits
-        offset
-    }
     
     def findValue(offset: Int): Int = {
     
